867a54b546e61bf200f70dde8a50d2f31f9a0e0d
casimirothompson <102702697+casimirothompson@users.noreply.github.com>
2022-08-30 20:22:23 -0300

    Create esboço-do-site.js

---
+++ esboço-do-site.js
@@ -0,0 +1,7 @@
+function exibe_barra_de_menus(){
+    document.getElementById("MenuInteligente").style.visibility="visible";
+   }
+   
+   function some_barra_de_menus(){
+       document.getElementById("MenuInteligente").style.visibility="hidden";
+   }
